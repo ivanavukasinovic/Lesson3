eeb50097d4e9192467892d3e5e1ac01d0ade6d0e
Your Name <you@example.com>
2019-04-28 15:42:03 +0200

    Using if else statement to test different options in the exercise AreYouHappy

--- src/exercises/AreYouHappy.java
+++ src/exercises/AreYouHappy.java
@@ -1,7 +1,28 @@
 package exercises;
 
-/* See the diagram in the "doc" directory. */
+import javax.swing.JOptionPane;
 
 public class AreYouHappy {
+
+	public static void main(String[] args) {
+
+		String answer1 = JOptionPane.showInputDialog("Are you happy?");
+
+		if (answer1.equals("no")) {
+			String answer2 = JOptionPane.showInputDialog("Do you want to be happy?");
+			if (answer2.equals("yes")) {
+				JOptionPane.showMessageDialog(null, "Change something.");
+			} else if (answer2.equals("no")) {
+				JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing.");
+			} else {
+				JOptionPane.showMessageDialog(null, "Please enter yes/no answer.");
+			}
+		} else if (answer1.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing.");
+		} else {
+			JOptionPane.showMessageDialog(null, "Please enter yes/no answer.");
+		}
+
+	}
 
 }
