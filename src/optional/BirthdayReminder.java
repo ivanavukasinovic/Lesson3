7f88dd57c8c806917d3f428fb63cee69c11b1e6d
Your Name <you@example.com>
2019-04-28 15:52:44 +0200

    BirthdayReminder - using if else statement to show the birthday user wants

--- src/optional/BirthdayReminder.java
+++ src/optional/BirthdayReminder.java
@@ -1,25 +1,42 @@
 package optional;
 
+import javax.swing.JOptionPane;
+
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
-		
-		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
 
-		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		// 1. correct the birthdays for your family below
+		String momsBirthday = "March 24th";
+		String dadsBirthday = "May 3th";
+		String myBirthday = "February 11th";
+
+		// 2. Find out which birthday the user wants and and store their response in a
+		// variable
+		String answer = JOptionPane.showInputDialog("Which birthday do you want?");
+
 		// 3. Print out what the user typed
-		
+		JOptionPane.showMessageDialog(null, "You want to know when is " + answer + "'s birthday");
+
 		// 4. if user asked for "mom"
-			//print mom's birthday
+		// print mom's birthday
+		if (answer.equals("mom")) {
+			JOptionPane.showMessageDialog(null, answer + "'s birthday is on " + momsBirthday);
+		}
 		// 5. if user asked for "dad"
-			// print dad's birthday
+		// print dad's birthday
+		else if (answer.equals("dad")) {
+			JOptionPane.showMessageDialog(null, answer + "'s birthday is on " + dadsBirthday);
+		}
 		// 6. if user asked for your name
-			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
+		// print myBirthday
+		else if (answer.equals("ivana")) {
+			JOptionPane.showMessageDialog(null, answer + "'s birthday is on " + myBirthday);
+		}
+		// 7. otherwise print "Sorry, i don't remember that person's birthday!"
+		else {
+			JOptionPane.showMessageDialog(null, "Sorry, i don't remember that person's birthday!");
+		}
 
 	}
 }
