ae8b92245013d76204f34944dd804bca2b375b35
Your Name <you@example.com>
2019-04-28 15:55:47 +0200

    CountQuarters exercise

--- src/optional/CountQuarters.java
+++ src/optional/CountQuarters.java
@@ -1,20 +1,36 @@
 package optional;
 
+import javax.swing.JOptionPane;
+
 import junit.framework.TestCase;
 
-/* Write a method called countQuarters(int money) that calculates the number of cents left over 
+/* Complete the countQuarters() method so that it calculates the number of cents left over 
  * if you took out all the possible quarters. */
 
 public class CountQuarters extends TestCase {
 	
 	public void test() throws Exception {
-		assertEquals(2, countQuarters(64));
-		assertEquals(3, countQuarters(1278));
+		assertEquals(0, countQuarters(0.25));
+		assertEquals(0, countQuarters(1));
+		assertEquals(14, countQuarters(0.64));
+		assertEquals(3, countQuarters(12.78));
 	}
 
-	private Object countQuarters(int i) {
+	private Object countQuarters(double money) {
 		// TODO Auto-generated method stub
-		return null;
+		if(money >= 0.25) {
+			return money*100 % 25;
+		}else {
+		return ("You have less than a quarter.");
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		
+		CountQuarters cq = new CountQuarters();
+		
+		cq.test();
+		
 	}
 
 }
