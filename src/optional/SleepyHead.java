f914a0c05f5a16ec26852ef8d10818962813dff3
Your Name <you@example.com>
2019-04-28 15:59:26 +0200

    SleepyHead - using boolean, and if else statement

--- src/optional/SleepyHead.java
+++ src/optional/SleepyHead.java
@@ -1,22 +1,45 @@
 package optional;
 
+import javax.swing.JOptionPane;
+
 public class SleepyHead {
 
 	public static void main(String[] args) {
-		
+
 		boolean isWeekday, isVacation;
-		
-		/*
-		 * Ask the user for these values using
-		 * JOptionPane.showConfirmDialog(null, "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
-		 */
-	
 
 		/*
-		 * Print “sleep in” if it is a vacation or a weekend. 
-		 * If it’s a weekday, print "get up lazybones!".
-		 * If it is a weekday, and we are on vacation, print “sleep in”.
+		 * Ask the user for these values using JOptionPane.showConfirmDialog(null,
+		 * "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
 		 */
+		/*
+		 * Print sleep in if it is a vacation or a weekend. If it is a weekday, print
+		 * "get up lazy bones!". If it is a weekday, and we are on vacation, print sleep
+		 * in.
+		 */
+
+		int answerWeekday = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		// System.out.println(answerWeekday);
+		if (answerWeekday == 0) {
+			isWeekday = true;
+		} else {
+			isWeekday = false;
+		}
+
+		int answerVacation = JOptionPane.showConfirmDialog(null, "Are you on vacation?", "", JOptionPane.YES_NO_OPTION);
+		if (answerVacation == 0) {
+			isVacation = true;
+		} else {
+			isVacation = false;
+		}
+
+		if (isVacation == true || isWeekday == false) {
+			JOptionPane.showMessageDialog(null, "Sleep in.");
+		} else if (isVacation == false || isWeekday == true) {
+			JOptionPane.showMessageDialog(null, "Get up lazy bones!");
+		} else if (isWeekday == true && isVacation == true) {
+			JOptionPane.showMessageDialog(null, "Sleep in.");
+		} 
+
 	}
 }
-
