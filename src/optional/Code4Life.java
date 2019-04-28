879cf8c1177a3288d1142248cd1f5e95ffc56f51
Your Name <you@example.com>
2019-04-28 15:54:26 +0200

    Code4Life - using if else statement

--- src/optional/Code4Life.java
+++ src/optional/Code4Life.java
@@ -5,6 +5,7 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
 	/*
@@ -12,11 +13,26 @@ public class Code4Life {
 	 * 
 	 * 1. If it's 3 or more, tell them they're a Code Ninja.
 	 * 
-	 * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
+	 * 2. If it's less than 2, tell them to stop watching YouTube and write code
+	 * instead.
 	 * 
 	 * 3. If it's more than 5, play the Batman theme song.
 	 */
-	
+
+	public static void main(String[] args) {
+
+		String answerAsString = JOptionPane.showInputDialog("How many hours have you spent coding this week?");
+		int answerAsInt = Integer.parseInt(answerAsString);
+
+		if (answerAsInt <= 2) {
+			JOptionPane.showMessageDialog(null, "Stop watching YouTube and write code instead.");
+		} else if (answerAsInt >= 3 && answerAsInt <= 5) {
+			JOptionPane.showMessageDialog(null, "You're a Code Ninja.");
+		} else {
+			Code4Life.playBatmanTheme();
+		}
+
+	}
 
 	private static void playBatmanTheme() {
 		try {
@@ -31,4 +47,3 @@ public class Code4Life {
 	}
 
 }
-
