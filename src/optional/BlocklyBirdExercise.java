8ea0068ea16d0eb1b2a84d67679d783221bc306f
Your Name <you@example.com>
2019-05-11 21:35:29 +0200

    BlocklyBird game completed

--- src/optional/BlocklyBirdExercise.java
+++ src/optional/BlocklyBirdExercise.java
@@ -4,4 +4,110 @@ package optional;
 *			bit.ly/bird-puzzle
 */
 
-public class BlocklyBirdExercise {}
+public class BlocklyBirdExercise {
+	
+	/*LEVEL 1*
+	 * 
+	 * heading(30);
+	 * 
+	 *LEVEL 2*
+	 * 
+	 * if (noWorm()) {
+	 * heading(0)
+	 * } else {
+	 * heading(90);
+	 * }
+	 * 
+	 *LEVEL 3*
+	 *
+	 *if (noWorm()) {
+	 *heading(300);
+	 *} else {
+	 *heading(60);
+	 *}
+	 * 
+	 *LEVEL 4*
+	 *
+	 *if (getX() > 70) {
+	 *heading(270);
+	 *} else {
+	 *heading(0);
+	 *}
+	 * 
+	 *LEVEL 5*
+	 *
+	 *if (getY() > 20) {
+	 *heading(270);
+	 *} else if (getY() < 20) {
+	 *heading(270);
+	 *} else {
+	 *heading(180);
+	 *}
+	 * 
+	 *LEVEL 6*
+	 *
+	 *if (noWorm()) {
+	 *heading(345);
+	 *} else if (getY() < 80) {
+	 *heading(90);
+	 *} else {
+	 *heading(180);
+	 *}
+	 *
+	 *LEVEL 7*
+	 *
+	 *if (getY() > 60) {
+	 *heading(225);
+	 *} else if (getY() > 20) {
+	 *heading(285);
+	 *} else if (getX() > 20) {
+	 *heading(180);
+	 *}
+	 * 
+	 *LEVEL 8*
+	 *
+	 *if (noWorm() && getY() < 40) {
+	 *heading(90);
+	 *} else if (noWorm() && getX() < 80) {
+	 *heading(0);
+	 *} else if (noWorm()) {
+	 *heading(270);
+	 *} else if (getY() < 70) {
+	 *heading(120);
+	 *} else {
+	 *heading(0);
+	 *}
+	 *
+	 *LEVEL 9*
+	 *
+	 *if (getX() > 20 && noWorm()) {
+	 *heading(180);
+	 *} else if (noWorm()) {
+	 *heading(270);
+	 *} else if (getX() < 40 && getY() < 60) {
+	 *heading(90);
+	 *} else if (getX() < 45) {
+	 *heading(0);
+	 *} else {
+	 *heading(315);
+	 *}
+	 *
+	 *LEVEL 10*
+	 *
+	 *if (getY() < 75 && getX() < 50 && noWorm()) {
+	 *heading(90);
+	 *} else if (getX() < 80 && noWorm()) {
+	 *heading(0);
+	 *} else if (noWorm()) {
+	 *heading(270);
+	 *} else if (getY() < 80 && getX() > 60) {
+	 *heading(90);
+	 *} else if (getX() > 20) {
+	 *heading(180);
+	 *} else {
+	 *heading(270);
+	 *}
+	 *
+	 */
+	
+}
